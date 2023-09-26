3e65ae3b5d8f1bfe53fa9c743cf8c8add6bf63cb
nmahendran-aws <75538437+nmahendran-aws@users.noreply.github.com>
2023-09-26 16:21:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~>3.0"
+      source = "hashicorp/aws"
+      version = "5.17.0"
     }
   }
 }
@@ -11,7 +11,7 @@ provider "aws" {
   region = var.region
   default_tags {
     tags = {
-      Owner       = "Globomantics"
+      Owner       = "Mahendran"
       Project     = var.project
       Environment = var.environment
     }
