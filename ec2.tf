f034127b75c3d18359066fd772f6f76ab9a90d1f
Mahendran Natarajan <mahendran.natarajan.aws@gmail.com>
2023-09-26 18:24:16 -0400

    Ec2

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "mahen-tf-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = module.vpc.public_subnets.subnet_id
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
