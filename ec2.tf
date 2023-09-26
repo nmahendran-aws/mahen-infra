221206ab7c00c0ece290ba8004085463e7b571c9
nmahendran-aws <75538437+nmahendran-aws@users.noreply.github.com>
2023-09-26 18:35:21 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,6 @@ module "ec2_instance" {
   name = "mahen-tf-instance"
 
   instance_type          = "t2.micro"
-  key_name               = "user1"
   monitoring             = true
   subnet_id              = element(module.vpc.public_subnets, 0)
   tags = {
