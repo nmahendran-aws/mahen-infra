2631a43f3564a31c9f03f415b0a070ff79008a89
Mahendran Natarajan <mahendran.natarajan.aws@gmail.com>
2023-09-26 18:33:26 -0400

    sg

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,6 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   key_name               = "user1"
   monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
   subnet_id              = element(module.vpc.public_subnets, 0)
   tags = {
     Terraform   = "true"
