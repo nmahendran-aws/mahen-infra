914c06eb927ce2e6a54ac00a3b6bdbcc0b56f0ce
nmahendran-aws <75538437+nmahendran-aws@users.noreply.github.com>
2023-09-26 16:23:16 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,10 @@ variable "environment" {
   description = "(Optional) Application environment for deployment, defaults to development."
   default     = "development"
 }
+
+variable "region" {
+  type        = string
+  description = "(Optional) The region where the resources are created. Defaults to us-east-1."
+  default     = "us-east-1"
+}
+
