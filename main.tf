08b110094ea96851811a69b71a01aef2da27bc72
Mahendran Natarajan <mahendran.natarajan.aws@gmail.com>
2023-09-26 16:19:35 -0400

    vpc

--- main.tf
+++ main.tf
@@ -17,3 +17,33 @@ provider "aws" {
     }
   }
 }
+
+resource "aws_vpc" "diamond_dogs" {
+  cidr_block           = var.address_space
+  enable_dns_hostnames = true
+
+  tags = {
+    name        = "${var.prefix}-vpc-${var.region}"
+    environment = var.environment
+  }
+}
+
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "mahen-tf-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
