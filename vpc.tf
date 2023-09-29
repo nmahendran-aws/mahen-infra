7477a1d8f5eba1fbfae6d0decf75dc146dbcc5d8
nmahendran-aws <75538437+nmahendran-aws@users.noreply.github.com>
2023-09-29 14:51:48 -0400

    subnet-names

--- vpc.tf
+++ vpc.tf
@@ -7,6 +7,8 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnet_names = ["private-subnet1","private-subnet2"]
+  public_subnet_names = ["public-subnet1","public-subnet2"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
