a58fdc54e7762bf0a88b224c4f8628ef49bf9bd8
Mahendran Natarajan <mahendran.natarajan.aws@gmail.com>
2023-09-30 09:43:09 -0400

    ec2

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,13 @@
+data "aws_subnets" "public1" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+  tags = {
+    Name = "public-subnet1"
+  }
+}
+
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
@@ -5,9 +15,10 @@ module "ec2_instance" {
 
   instance_type          = "t2.micro"
   monitoring             = true
-  subnet_id              = element(module.vpc.public_subnets, 0)
+  subnet_id              = data.aws_subnets.public1.id
   tags = {
     Terraform   = "true"
     Environment = "dev"
+    Subnet = "public-subnet1"
   }
 }
--- variables.tf
+++ variables.tf
@@ -15,3 +15,9 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "vpc_id" {
+  type        = string
+  description = "(Required) The region where the resources are created. Defaults to us-east-1."
+  default     = "vpc-00a915c5d2c6d0c4c"
+}
+
