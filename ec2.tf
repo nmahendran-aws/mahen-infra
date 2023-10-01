1888abeb785e7258eb4d3febecf3437affbc8d82
nmahendran-aws <75538437+nmahendran-aws@users.noreply.github.com>
2023-10-01 15:21:07 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -47,7 +47,7 @@ module "ec2_instance" {
   user_data                     = var.user_data
   key_name                      = "nmahendran-kp"
   associate_public_ip_address   = true
-  vpc_security_group_ids = [aws_security_group.nstance-sg.id]
+  vpc_security_group_ids = [aws_security_group.instance-sg.id]
   tags = {
     Terraform   = "true"
     Environment = "dev"
