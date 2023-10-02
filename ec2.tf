48b588e7598cc727897891117a4161f6055f4106
Mahendran Natarajan <mahendran.natarajan.aws@gmail.com>
2023-10-02 14:49:53 -0400

    sh

--- ec2.tf
+++ ec2.tf
@@ -52,13 +52,7 @@ module "ec2_instance" {
     Environment = "dev"
     Subnet = "public-subnet1"
   }
-  user_data = <<EOF
-#! /bin/bash
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server 2</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
+  user_data = templatefile("${path.module}/files/deploy_app.sh")
 depends_on = [
     aws_security_group.instance-sg
   ]
