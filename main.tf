9bdd92dbd16e5f52e138cb318215dfcdbb6782b5
nmahendran-aws <75538437+nmahendran-aws@users.noreply.github.com>
2024-12-16 14:42:22 -0500

    github action

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "remote" {
+         organization = "Mahen-AWS"
+         workspaces {
+           name = "mahen-infra"
+         }
+       }
 }
 
 provider "aws" {
@@ -15,4 +21,10 @@ provider "aws" {
 data "aws_ssm_parameter" "amzn2_linux" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
+
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+    }
+  }
 
